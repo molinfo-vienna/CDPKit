d19f5378a0623589faf41828220a0e94e62e04f7
Thomas Seidel <thomas.seidel@univie.ac.at>
2020-02-28 09:58:18 +0100

    Updated copyright notice

--- Libs/C++/Source/CDPL/ForceField/MMFF94FormalAtomChargeDefinitionTable.cpp
+++ Libs/C++/Source/CDPL/ForceField/MMFF94FormalAtomChargeDefinitionTable.cpp
@@ -5,7 +5,7 @@
  *
  * This file is part of the Chemical Data Processing Toolkit
  *
- * Copyright (C) 2003-2010 Thomas A. Seidel <thomas.seidel@univie.ac.at>
+ * Copyright (C) 2003-2020 Thomas A. Seidel <thomas.seidel@univie.ac.at>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
