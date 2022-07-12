3403feea1b25a4313971d01028060878e71f15a7
Thomas Seidel <thomas.seidel@univie.ac.at>
2022-07-12 15:24:25 +0200

    Documentation updates

---
+++ Doc/Doxygen/Python-API/CDPL/Vis/CairoBackend.py
@@ -0,0 +1,388 @@
+# -*- mode: python; tab-width: 4 -*-
+
+## 
+# CairoBackend.py 
+#
+# This file is part of the Chemical Data Processing Toolkit
+#
+# Copyright (C) 2003-2020 Thomas A. Seidel <thomas.seidel@univie.ac.at>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; see the file COPYING. If not, write to
+# the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+##
+
+
+from __future__ import absolute_import
+
+import array
+import cairo
+import math
+
+import CDPL.Vis
+
+
+__all__ = [ 'CairoFontMetrics', 'CairoRenderer2D' ]
+
+
+dense1PatternData = array.array('B', [ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255 ])
+dense2PatternData = array.array('B', [ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 255, 255, 255, 255, 255 ])
+dense3PatternData = array.array('B', [ 255, 0, 255, 255, 0, 255, 0, 255, 255, 255, 255, 0, 0, 255, 0, 255 ])
+dense4PatternData = array.array('B', [ 255, 0, 255, 0, 0, 255, 0, 255, 255, 0, 255, 0, 0, 255, 0, 255 ])
+dense5PatternData = array.array('B', [ 0, 255, 0, 0, 255, 0, 255, 0, 0, 0, 0, 255, 255, 0, 255, 0 ])
+dense6PatternData = array.array('B', [ 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0 ])
+dense7PatternData = array.array('B', [ 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ])
+horPatternData = array.array('B', [ 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0 ])
+verPatternData = array.array('B', [ 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0 ])
+crossPatternData = array.array('B', [ 0, 255, 0, 0, 255, 255, 255, 255, 0, 255, 0, 0, 0, 255, 0, 0 ])
+leftDiagPatternData = array.array('B', [ 0, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 0 ])
+rightDiagPatternData = array.array('B', [ 255, 0, 0, 0, 0, 255, 0, 0, 0, 0, 255, 0, 0, 0, 0, 255 ])
+diagCrossPatternData = array.array('B', [ 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 255, 0, 255, 0, 0, 0,
+										  0, 255, 0, 0, 0, 255, 0, 0, 255, 0, 0, 0, 0, 0, 255,
+										  0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 0, 0, 0, 0, 0, 255, 0,
+										  0, 255, 0, 0, 0, 255, 0, 0, 0, 0, 255, 0, 255, 0, 0, 0 ])
+
+dense1Pattern    = cairo.ImageSurface.create_for_data(dense1PatternData,    cairo.FORMAT_A8, 4, 4, 4) 
+dense2Pattern    = cairo.ImageSurface.create_for_data(dense2PatternData,    cairo.FORMAT_A8, 4, 4, 4) 
+dense3Pattern    = cairo.ImageSurface.create_for_data(dense3PatternData,    cairo.FORMAT_A8, 4, 4, 4) 
+dense4Pattern    = cairo.ImageSurface.create_for_data(dense4PatternData,    cairo.FORMAT_A8, 4, 4, 4) 
+dense5Pattern    = cairo.ImageSurface.create_for_data(dense5PatternData,    cairo.FORMAT_A8, 4, 4, 4) 
+dense6Pattern    = cairo.ImageSurface.create_for_data(dense6PatternData,    cairo.FORMAT_A8, 4, 4, 4) 
+dense7Pattern    = cairo.ImageSurface.create_for_data(dense7PatternData,    cairo.FORMAT_A8, 4, 4, 4) 
+horPattern       = cairo.ImageSurface.create_for_data(horPatternData,       cairo.FORMAT_A8, 4, 4, 4) 
+verPattern       = cairo.ImageSurface.create_for_data(verPatternData,       cairo.FORMAT_A8, 4, 4, 4) 
+crossPattern     = cairo.ImageSurface.create_for_data(crossPatternData,     cairo.FORMAT_A8, 4, 4, 4) 
+leftDiagPattern  = cairo.ImageSurface.create_for_data(leftDiagPatternData,  cairo.FORMAT_A8, 4, 4, 4) 
+rightDiagPattern = cairo.ImageSurface.create_for_data(rightDiagPatternData, cairo.FORMAT_A8, 4, 4, 4) 
+diagCrossPattern = cairo.ImageSurface.create_for_data(diagCrossPatternData, cairo.FORMAT_A8, 8, 8, 8)
+
+fillPatternMap = { CDPL.Vis.Brush.DENSE1_PATTERN     : dense1Pattern,
+				   CDPL.Vis.Brush.DENSE2_PATTERN     : dense2Pattern,
+				   CDPL.Vis.Brush.DENSE3_PATTERN     : dense3Pattern,
+				   CDPL.Vis.Brush.DENSE4_PATTERN     : dense4Pattern,
+				   CDPL.Vis.Brush.DENSE5_PATTERN     : dense5Pattern,
+				   CDPL.Vis.Brush.DENSE6_PATTERN     : dense6Pattern,
+				   CDPL.Vis.Brush.DENSE7_PATTERN     : dense7Pattern,
+				   CDPL.Vis.Brush.H_PATTERN          : horPattern,
+				   CDPL.Vis.Brush.V_PATTERN          : verPattern,
+				   CDPL.Vis.Brush.CROSS_PATTERN      : crossPattern,
+				   CDPL.Vis.Brush.LEFT_DIAG_PATTERN  : leftDiagPattern,
+				   CDPL.Vis.Brush.RIGHT_DIAG_PATTERN : rightDiagPattern,
+				   CDPL.Vis.Brush.DIAG_CROSS_PATTERN : diagCrossPattern }
+
+capStyleMap = { CDPL.Vis.Pen.FLAT_CAP   : cairo.LINE_CAP_BUTT,
+				CDPL.Vis.Pen.SQUARE_CAP : cairo.LINE_CAP_SQUARE,
+				CDPL.Vis.Pen.ROUND_CAP  : cairo.LINE_CAP_ROUND } 
+
+joinStyleMap = { CDPL.Vis.Pen.MITER_JOIN : cairo.LINE_JOIN_MITER,
+				 CDPL.Vis.Pen.BEVEL_JOIN : cairo.LINE_JOIN_BEVEL,
+				 CDPL.Vis.Pen.ROUND_JOIN : cairo.LINE_JOIN_ROUND }
+
+
+class CairoFontMetrics(CDPL.Vis.FontMetrics):
+
+	def __init__(self, cairo_ctxt):
+		"__init__(CairoFontMetrics self, cairo.Context cairo_ctxt) -> None :"
+		CDPL.Vis.FontMetrics.__init__(self)
+		self.__cairoContext = cairo_ctxt
+
+	def setFont(self, font):
+		"setFont(CairoFontMetrics self, Vis.Font font) -> None :"
+		if font.italic:
+			font_slant = cairo.FONT_SLANT_ITALIC
+		else:
+			font_slant = cairo.FONT_SLANT_NORMAL
+
+		if font.bold:
+			font_weight = cairo.FONT_WEIGHT_BOLD
+		else:
+			font_weight = cairo.FONT_WEIGHT_NORMAL
+			
+		self.__cairoContext.select_font_face(font.family, font_slant, font_weight)
+		self.__cairoContext.set_font_size(font.size)
+
+		self.__fontExtents = self.__cairoContext.font_extents()
+		
+	def getAscent(self):
+		"getAscent(CairoFontMetrics self) -> float :"
+		return self.__fontExtents[0]
+	
+	def getDescent(self):
+		"getDescent(CairoFontMetrics self) -> float :"
+		return self.__fontExtents[1]
+	
+	def getHeight(self):
+		"getHeight(CairoFontMetrics self) -> float :"
+		return (self.getAscent() + self.getDescent() + 1.0)
+	
+	def getLeading(self):
+		"getLeading(CairoFontMetrics self) -> float :"
+		return (self.__fontExtents[2] - self.getHeight())
+	
+	def getWidth(self, string):
+		"getWidth(CairoFontMetrics self) -> float :"
+		return self.__cairoContext.text_extents(string)[4]
+
+	def getBounds(self, string, bounds):
+		"getBounds(CairoFontMetrics self, str string, Vis.Rectangle2D bounds) -> float :"
+		text_extents = self.__cairoContext.text_extents(string)
+
+		bounds.setBounds(text_extents[0], text_extents[1],
+						 text_extents[2] + text_extents[0],
+						 text_extents[3] + text_extents[1])
+
+
+class CairoRenderer2D(CDPL.Vis.Renderer2D):
+
+	def __init__(self, cairo_ctxt):
+		"__init__(CairoRenderer2D self, cairo.Context cairo_ctxt) -> None :"
+		CDPL.Vis.Renderer2D.__init__(self)
+		self.__cairoContext = cairo_ctxt
+		self.__penStack = [ CDPL.Vis.Pen() ]
+		self.__brushStack = [ CDPL.Vis.Brush() ]
+		self.__fontStack = [ CDPL.Vis.Font() ]
+
+	def saveState(self):
+		"saveState(CairoRenderer2D self) -> None :"
+		self.__cairoContext.save()
+
+		self.__penStack.append(self.__penStack[-1])
+		self.__brushStack.append(self.__brushStack[-1])
+		self.__fontStack.append(self.__fontStack[-1])
+
+	def restoreState(self):
+		"restoreState(CairoRenderer2D self) -> None :"
+		if len(self.__penStack) > 1:
+			self.__cairoContext.restore()
+
+			del self.__penStack[-1]
+			del self.__brushStack[-1]
+			del self.__fontStack[-1]
+
+	def setTransform(self, xform):
+		"setTransform(CairoRenderer2D self, Math.AffineTransform3D xform) -> None :"
+		cairo_xform = cairo.Matrix(xform(0, 0), xform(1, 0), xform(0, 1), 
+								   xform(1, 1), xform(0, 2), xform(1, 2))
+
+		self.__cairoContext.set_matrix(cairo_xform)
+
+	def transform(self, xform):
+		"transform(CairoRenderer2D self, Math.AffineTransform3D xform) -> None :"
+		cairo_xform = cairo.Matrix(xform(0, 0), xform(1, 0), xform(0, 1), 
+								   xform(1, 1), xform(0, 2), xform(1, 2))
+	
+		self.__cairoContext.transform(cairo_xform)
+
+	def setPen(self, pen):
+		"setPen(CairoRenderer2D self, Vis.Pen pen) -> None :"
+		self.__penStack[-1] = pen
+
+	def setBrush(self, brush):
+		"setBrush(CairoRenderer2D self, Vis.Brush brush) -> None :"
+		self.__brushStack[-1] = brush
+
+	def setFont(self, font):
+		"setFont(CairoRenderer2D self, Vis.Font font) -> None :"
+		self.__fontStack[-1] = font
+
+	def drawRectangle(self, x, y, width, height):
+		"drawRectangle(CairoRenderer2D self, float x, float y, float width, float height) -> None :"
+		self.__cairoContext.new_path()
+		self.__cairoContext.rectangle(x, y, width, height)
+
+		self.__fillPath()
+		self.__strokePath()
+
+	def drawPolygon(self, points):
+		"drawPolygon(CairoRenderer2D self, Math.Vector2DArray points) -> None :"
+		self.__cairoContext.new_path()
+
+		move = True
+
+		for point in points:
+			if move:
+				self.__cairoContext.move_to(point[0], point[1])
+				move = False
+			else:
+				self.__cairoContext.line_to(point[0], point[1])
+
+		self.__cairoContext.close_path()
+
+		self.__fillPath()
+		self.__strokePath()
+
+	def drawLine(self, x1, y1, x2, y2):
+		"drawLine(CairoRenderer2D self, float x1, float y1, float x2, float y2) -> None :"
+		self.__cairoContext.new_path()
+
+		self.__cairoContext.move_to(x1, y1)
+		self.__cairoContext.line_to(x2, y2)
+
+		self.__strokePath()
+
+	def drawPolyline(self, points):
+		"drawPolyline(CairoRenderer2D self, Math.Vector2DArray points) -> None :"
+		self.__cairoContext.new_path()
+
+		move = True
+		
+		for point in points:
+			if move:
+				self.__cairoContext.move_to(point[0], point[1])
+				move = False
+			else:
+				self.__cairoContext.line_to(point[0], point[1])
+
+		self.__strokePath()
+
+	def drawLineSegments(self, points):
+		"drawLineSegments(CairoRenderer2D self, Math.Vector2DArray points) -> None :"
+		self.__cairoContext.new_path()
+
+		for i in range(0, len(points) / 2):
+			point1 = points[i * 2]
+			point2 = points[i * 2 + 1]
+
+			self.__cairoContext.move_to(point1[0], point1[1])
+			self.__cairoContext.line_to(point2[0], point2[1])
+
+		self.__strokePath()
+
+	def drawPoint(self, x, y):
+		"drawPoint(CairoRenderer2D self, float x, float y) -> None :"
+		self.__cairoContext.new_path()
+
+		radius = self.__penStack[-1].width * 0.5
+		color = self.__penStack[-1].color
+
+		self.__cairoContext.set_source_rgba(color.red, color.green, color.blue, color.alpha)
+
+		self.__cairoContext.arc(x, y, radius, 0.0, 2.0 * math.pi)
+		self.__cairoContext.fill()
+
+	def drawEllipse(self, x, y, width, height):
+		"drawEllipse(CairoRenderer2D self, float x, float y, float width, float height) -> None :"
+		self.__cairoContext.save()
+
+		self.__cairoContext.new_path()
+
+		self.__cairoContext.translate(x, y);
+		self.__cairoContext.scale(1.0, height / width);
+		self.__cairoContext.arc(0.0, 0.0, width * 0.5, 0.0, 2 * 3.141593)
+
+		self.__fillPath();
+		self.__strokePath();
+
+		self.__cairoContext.restore()
+
+	def drawText(self, x, y, txt):
+		"drawText(CairoRenderer2D self, float x, float y, str txt) -> None :"
+		font = self.__fontStack[-1]
+		
+		if font.italic:
+			font_slant = cairo.FONT_SLANT_ITALIC
+		else:
+			font_slant = cairo.FONT_SLANT_NORMAL
+
+		if font.bold:
+			font_weight = cairo.FONT_WEIGHT_BOLD
+		else:
+			font_weight = cairo.FONT_WEIGHT_NORMAL
+			
+		self.__cairoContext.select_font_face(font.family, font_slant, font_weight)
+		self.__cairoContext.set_font_size(font.size)
+
+		color = self.__penStack[-1].color
+
+		self.__cairoContext.set_source_rgba(color.red, color.green, color.blue, color.alpha)
+		self.__cairoContext.move_to(x, y)
+		self.__cairoContext.show_text(txt)
+
+	def __fillPath(self):
+		brush = self.__brushStack[-1]
+
+		if brush.style == CDPL.Vis.Brush.NO_PATTERN:
+			return
+
+		color = brush.color
+
+		try:
+			pattern = fillPatternMap[brush.style]
+		except KeyError:
+			self.__cairoContext.set_source_rgba(color.red, color.green, color.blue, color.alpha)
+			self.__cairoContext.fill_preserve()
+			return
+	
+		ptn_width = pattern.get_width()
+		ptn_height = pattern.get_height()
+
+		src_surf = cairo.ImageSurface(cairo.FORMAT_ARGB32, ptn_width, ptn_height)
+		src_ctxt = cairo.Context(src_surf)
+
+		src_ctxt.set_source_rgba(color.red, color.green, color.blue, color.alpha)
+		src_ctxt.mask_surface(pattern, 0.0, 0.0)
+
+		src_pattern = cairo.Pattern(src_surf)
+		src_pattern.set_extend(cairo.EXTEND_REPEAT)
+
+		self.__cairoContext.set_source(src_pattern)
+		self.__cairoContext.fill_preserve()
+
+	def __strokePath(self):
+		pen = self.__penStack[-1]
+		line_width = pen.width
+
+		if line_width == 0.0:
+			self.__cairoContext.set_dash([], 0)
+		else:
+			line_style = pen.lineStyle
+			
+			if line_style == CDPL.Vis.Pen.NO_LINE:
+				return
+	
+			elif line_style == CDPL.Vis.Pen.DASH_LINE:
+				self.__cairoContext.set_dash([ 5.0 * line_width ], 0)
+
+			elif line_style == CDPL.Vis.Pen.DOT_LINE:
+				self.__cairoContext.set_dash([ line_width, line_width * 1.5 ], 0)
+
+			elif line_style == CDPL.Vis.Pen.DASH_DOT_LINE:
+				self.__cairoContext.set_dash([ line_width * 5.0, line_width * 1.5,
+											   line_width, line_width * 1.5 ], 0)
+
+			elif line_style == CDPL.Vis.Pen.DASH_DOT_DOT_LINE:
+				self.__cairoContext.set_dash([ line_width * 5.0, line_width * 1.5,
+											   line_width, line_width * 1.5,
+											   line_width, line_width * 1.5 ], 0)
+			else:
+				self.__cairoContext.set_dash([], 0)
+
+		try:
+			join_style = joinStyleMap[pen.joinStyle]
+		except KeyError:
+			join_style = cairo.LINE_JOIN_ROUND
+	
+		try:
+			cap_style = capStyleMap[pen.capStyle]
+		except KeyError:
+			cap_style = cairo.LINE_CAP_ROUND
+
+		color = pen.color
+		
+		self.__cairoContext.set_line_join(join_style)
+		self.__cairoContext.set_line_cap(cap_style)
+		self.__cairoContext.set_line_width(line_width)
+		self.__cairoContext.set_source_rgba(color.red, color.green, color.blue, color.alpha)
+
+		self.__cairoContext.stroke()
---
+++ Doc/Doxygen/Python-API/CDPL/Vis/QtBackend.py
@@ -0,0 +1,262 @@
+# -*- mode: python; tab-width: 4 -*-
+
+## 
+# QtBackend.py 
+#
+# This file is part of the Chemical Data Processing Toolkit
+#
+# Copyright (C) 2003-2020 Thomas A. Seidel <thomas.seidel@univie.ac.at>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; see the file COPYING. If not, write to
+# the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+##
+
+
+from __future__ import absolute_import
+
+import PyQt5.QtCore as QtCore
+import PyQt5.QtGui as QtGui
+
+import CDPL.Vis
+
+
+__all__ = [ 'QtFontMetrics', 'QtRenderer2D', 'QtObjectFactory' ]
+
+
+lineStyleMap = { CDPL.Vis.Pen.DASH_LINE         : QtCore.Qt.DashLine,
+				 CDPL.Vis.Pen.DOT_LINE          : QtCore.Qt.DotLine,
+				 CDPL.Vis.Pen.DASH_DOT_LINE     : QtCore.Qt.DashDotLine,
+				 CDPL.Vis.Pen.DASH_DOT_DOT_LINE : QtCore.Qt.DashDotDotLine,
+				 CDPL.Vis.Pen.SOLID_LINE        : QtCore.Qt.SolidLine,
+				 CDPL.Vis.Pen.NO_LINE           : QtCore.Qt.NoPen }
+
+capStyleMap = { CDPL.Vis.Pen.FLAT_CAP   : QtCore.Qt.FlatCap,
+				CDPL.Vis.Pen.SQUARE_CAP : QtCore.Qt.SquareCap,
+				CDPL.Vis.Pen.ROUND_CAP  : QtCore.Qt.RoundCap } 
+
+joinStyleMap = { CDPL.Vis.Pen.MITER_JOIN : QtCore.Qt.MiterJoin,
+				 CDPL.Vis.Pen.BEVEL_JOIN : QtCore.Qt.BevelJoin,
+				 CDPL.Vis.Pen.ROUND_JOIN : QtCore.Qt.RoundJoin }
+
+brushStyleMap = { CDPL.Vis.Brush.SOLID_PATTERN      : QtCore.Qt.SolidPattern, 
+				  CDPL.Vis.Brush.DENSE1_PATTERN     : QtCore.Qt.Dense1Pattern, 
+				  CDPL.Vis.Brush.DENSE2_PATTERN     : QtCore.Qt.Dense2Pattern, 
+				  CDPL.Vis.Brush.DENSE3_PATTERN     : QtCore.Qt.Dense3Pattern, 
+				  CDPL.Vis.Brush.DENSE4_PATTERN     : QtCore.Qt.Dense4Pattern,
+				  CDPL.Vis.Brush.DENSE5_PATTERN     : QtCore.Qt.Dense5Pattern,
+				  CDPL.Vis.Brush.DENSE6_PATTERN     : QtCore.Qt.Dense6Pattern,
+				  CDPL.Vis.Brush.DENSE7_PATTERN     : QtCore.Qt.Dense7Pattern,
+				  CDPL.Vis.Brush.H_PATTERN          : QtCore.Qt.HorPattern,
+				  CDPL.Vis.Brush.V_PATTERN          : QtCore.Qt.VerPattern,
+				  CDPL.Vis.Brush.CROSS_PATTERN      : QtCore.Qt.CrossPattern,
+				  CDPL.Vis.Brush.LEFT_DIAG_PATTERN  : QtCore.Qt.BDiagPattern,
+				  CDPL.Vis.Brush.RIGHT_DIAG_PATTERN : QtCore.Qt.FDiagPattern,
+				  CDPL.Vis.Brush.DIAG_CROSS_PATTERN : QtCore.Qt.DiagCrossPattern,
+				  CDPL.Vis.Brush.NO_PATTERN         : QtCore.Qt.NoBrush }
+
+
+class QtObjectFactory(object):
+
+	def createQFont(font):
+		"createQFont(Vis.Font font) -> PyQt4.QtGui.QFont :"
+		qt_font = QtGui.QFont(font.family)
+
+		if (font.size <= 1.0):
+			qt_font.setPointSizeF(1.0)
+		else:
+			qt_font.setPointSizeF(font.size)
+
+		qt_font.setBold(font.bold)
+		qt_font.setItalic(font.italic)
+		qt_font.setUnderline(font.underlined)
+		qt_font.setOverline(font.overlined)
+		qt_font.setStrikeOut(font.strikedOut)
+		qt_font.setFixedPitch(font.fixedPitch)
+
+		return qt_font
+
+	def createQColor(color):
+		"createQColor(Vis.Color color) -> PyQt4.QtGui.QColor :"
+		qt_color = QtGui.QColor()
+		
+		qt_color.setRedF(color.red)
+		qt_color.setGreenF(color.green)
+		qt_color.setBlueF(color.blue)
+		qt_color.setAlphaF(color.alpha)
+
+		return qt_color
+
+	def createQPen(pen):
+		"createQPen(Vis.Pen pen) -> PyQt4.QtGui.QPen :"
+		try:
+			pen_style = lineStyleMap[pen.lineStyle]
+		except KeyError:
+			pen_style = QtCore.QtGui.SolidLine
+
+		try:
+			cap_style = capStyleMap[pen.capStyle]
+		except KeyError:
+			cap_style = QtCore.QtGui.RoundCap
+
+		try:
+			join_style = joinStyleMap[pen.joinStyle]
+		except KeyError:
+			join_style = QtCore.QtGui.RoundJoin
+
+		qt_pen = QtGui.QPen(QtObjectFactory.createQColor(pen.color));
+
+		qt_pen.setStyle(pen_style)
+		qt_pen.setCapStyle(cap_style)
+		qt_pen.setJoinStyle(join_style)
+		qt_pen.setWidthF(pen.width)
+
+		return qt_pen
+
+	def createQBrush(brush):
+		"createQBrush(Vis.Brush brush) -> PyQt4.QtGui.QBrush :"
+		try:
+			brush_style = brushStyleMap[brush.style]
+		except KeyError:
+			brush_style = QtCore.QtGui.NoBrush
+
+		return QtGui.QBrush(QtObjectFactory.createQColor(brush.color), brush_style)
+
+	createQFont  = staticmethod(createQFont)
+	createQColor = staticmethod(createQColor)
+	createQPen   = staticmethod(createQPen)
+	createQBrush = staticmethod(createQBrush);
+
+
+class QtFontMetrics(CDPL.Vis.FontMetrics):
+
+	def __init__(self, paint_dev = None):
+		"__init__(QtFontMetrics self, PyQt4.QtGui.QPaintDevice paint_dev) -> None :"
+		CDPL.Vis.FontMetrics.__init__(self)
+
+		self.__qPaintDevice = paint_dev
+		self.__qFontMetrics = QtGui.QFontMetricsF(QtGui.QFont(), paint_dev)
+
+	def setFont(self, font):
+		"setFont(QtFontMetrics self, Vis.Font font) -> None :"
+		self.__qFontMetrics = QtGui.QFontMetricsF(QtObjectFactory.createQFont(font), self.__qPaintDevice)
+			
+	def getAscent(self):
+		"getAscent(QtFontMetrics self) -> float :"
+		return self.__qFontMetrics.ascent()
+	
+	def getDescent(self):
+		"getDescent(QtFontMetrics self) -> float :"
+		return self.__qFontMetrics.descent()
+	
+	def getHeight(self):
+		"getHeight(QtFontMetrics self) -> float :"
+		return self.__qFontMetrics.height()
+	
+	def getLeading(self):
+		"getLeading(QtFontMetrics self) -> float :"
+		return self.__qFontMetrics.leading()
+	
+	def getWidth(self, string):
+		"getWidth(QtFontMetrics self) -> float :"
+		return self.__qFontMetrics.width(string)
+
+	def getBounds(self, string, bounds):
+		"getBounds(QtFontMetrics self, str string, Vis.Rectangle2D bounds) -> float :"
+		str_bounds = self.__qFontMetrics.boundingRect(string)
+
+		bounds.setBounds(str_bounds.left(), str_bounds.top(), str_bounds.right(), str_bounds.bottom())
+
+	
+class QtRenderer2D(CDPL.Vis.Renderer2D):
+
+	def __init__(self, painter):
+		"__init__(QtRenderer2D self, cairo.Context cairo_ctxt) -> None :"
+		CDPL.Vis.Renderer2D.__init__(self)
+		self.__qPainter = painter
+
+	def saveState(self):
+		"saveState(QtRenderer2D self) -> None :"
+		self.__qPainter.save()
+	
+	def restoreState(self):
+		"restoreState(QtRenderer2D self) -> None :"
+		self.__qPainter.restore()
+	
+	def setTransform(self, xform):
+		"setTransform(QtRenderer2D self, Math.AffineTransform3D xform) -> None :"
+		self.__qPainter.setWorldTransform(QtGui.QTransform(xform(0, 0), xform(1, 0), xform(2, 0), 
+														   xform(0, 1), xform(1, 1), xform(2, 1), 
+														   xform(0, 2), xform(1, 2), xform(2, 2)), False)
+	
+	def transform(self, xform):
+		"transform(QtRenderer2D self, Math.AffineTransform3D xform) -> None :"
+		self.__qPainter.setWorldTransform(QtGui.QTransform(xform(0, 0), xform(1, 0), xform(2, 0), 
+														   xform(0, 1), xform(1, 1), xform(2, 1), 
+														   xform(0, 2), xform(1, 2), xform(2, 2)), True)
+
+	def setPen(self, pen):
+		"setPen(QtRenderer2D self, Vis.Pen pen) -> None :"
+		self.__qPainter.setPen(QtObjectFactory.createQPen(pen))
+	
+	def setBrush(self, brush):
+		"setBrush(QtRenderer2D self, Vis.Brush brush) -> None :"
+		self.__qPainter.setBrush(QtObjectFactory.createQBrush(brush))
+	
+	def setFont(self, font):
+		"setFont(QtRenderer2D self, Vis.Font font) -> None :"
+		self.__qPainter.setFont(QtObjectFactory.createQFont(font))
+	
+	def drawRectangle(self, x, y, width, height):
+		"drawRectangle(QtRenderer2D self, float x, float y, float width, float height) -> None :"
+		self.__qPainter.drawRect(QtGui.QRectF(x, y, width, height))
+	
+	def drawPolygon(self, points):
+		"drawPolygon(QtRenderer2D self, Math.Vector2DArray points) -> None :"
+		self.__qPainter.drawPolygon(self.__convertToQPolygon(points))
+			
+	def drawLine(self, x1, y1, x2, y2):
+		"drawLine(QtRenderer2D self, float x1, float y1, float x2, float y2) -> None :"
+		self.__qPainter.drawLine(QtGui.QPointF(x1, y1), QtGui.QPointF(x2, y2))
+	
+	def drawPolyline(self, points):
+		"drawPolyline(QtRenderer2D self, Math.Vector2DArray points) -> None :"
+		self.__qPainter.drawPolyline(self.__convertToQPolygon(points))
+	
+	def drawLineSegments(self, points):
+		"drawLineSegments(QtRenderer2D self, Math.Vector2DArray points) -> None :"
+		self.__qPainter.drawLines(self.__convertToQPolygon(points))
+	
+	def drawPoint(self, x, y):
+		"drawPoint(QtRenderer2D self, float x, float y) -> None :"
+		self.__qPainter.drawPoint(QtGui.QPointF(x, y))
+        
+	def drawEllipse(self, x, y, width, height):
+		"drawEllipse(QtRenderer2D self, float x, float y, float width, float height) -> None :"
+		self.__qPainter.drawEllipse(QtGui.QPointF(x, y), width * 0.5, height * 0.5)
+
+	def drawText(self, x, y, txt):
+		"drawText(QtRenderer2D self, float x, float y, str txt) -> None :"
+		self.__qPainter.drawText(QtGui.QPointF(x, y), txt)
+
+	def __convertToQPolygon(self, points):
+		if not hasattr(self.__qPolygon):
+			self.__qPolgyon = QtGui.QPolygonF()
+
+		self.__qPolygon.clear()
+
+		for point in points:
+			self.__qPolygon.append(QtGui.QPointF(point[0], point[1]))
+
+		return self.__qPolygon
