ae5006007427deb187ea8818b2b0713e42de0d96
Thomas Seidel <thomas.seidel@univie.ac.at>
2023-05-01 21:36:25 +0200

    Python API documentation updates

---
+++ Doc/Doxygen/Python-API/Source/CDPL/GRAILS/pkg_BuriednessScore.py
@@ -0,0 +1,149 @@
+#
+# This file is part of the Chemical Data Processing Toolkit
+#
+# Copyright (C) Thomas A. Seidel <thomas.seidel@univie.ac.at>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; see the file COPYING. If not, write to
+# the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
+
+##
+# \brief 
+#
+class BuriednessScore(Boost.Python.instance):
+
+    ##
+    # \brief 
+    #
+    DEF_NUM_TEST_RAYS = 200
+
+    ##
+    # \brief 
+    #
+    DEF_PROBE_RADIUS = 8.0
+
+    ##
+    # \brief Initializes the \e %BuriednessScore instance.
+    # \param score 
+    #
+    def __init__(score: BuriednessScore) -> None: pass
+
+    ##
+    # \brief Initializes the \e %BuriednessScore instance.
+    # \param probe_radius 
+    # \param min_vdw_surf_dist 
+    # \param num_test_rays 
+    #
+    def __init__(probe_radius: float = 8.0, min_vdw_surf_dist: float = 1.0, num_test_rays: float = 200) -> None: pass
+
+    ##
+    # \brief Returns the numeric identifier (ID) of the wrapped C++ class instance.
+    #
+    # Different Python \e %BuriednessScore instances may reference the same underlying C++ class instance. The commonly used Python expression
+    # <tt>a is not b</tt> thus cannot tell reliably whether the two \e %BuriednessScore instances \e a and \e b reference different C++ objects. 
+    # The numeric identifier returned by this method allows to correctly implement such an identity test via the simple expression
+    # <tt>a.getObjectID() != b.getObjectID()</tt>.
+    #
+    # \return The numeric ID of the internally referenced C++ class instance.
+    #
+    def getObjectID() -> int: pass
+
+    ##
+    # \brief 
+    # \param dist 
+    #
+    def setMinVdWSurfaceDistance(dist: float) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getMinVdWSurfaceDistance() -> float: pass
+
+    ##
+    # \brief 
+    # \param radius 
+    #
+    def setProbeRadius(radius: float) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getProbeRadius() -> float: pass
+
+    ##
+    # \brief 
+    # \param num_rays 
+    #
+    def setNumTestRays(num_rays: int) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getNumTestRays() -> int: pass
+
+    ##
+    # \brief 
+    # \param func 
+    #
+    def setAtom3DCoordinatesFunction(func: CDPL.Chem.Atom3DCoordinatesFunction) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getAtom3DCoordinatesFunction() -> CDPL.Chem.Atom3DCoordinatesFunction: pass
+
+    ##
+    # \brief Replaces the current state of \a self with a copy of the state of the \e %BuriednessScore instance \a score.
+    # \param score The \e %BuriednessScore instance to copy.
+    # \return \a self
+    #
+    def assign(score: BuriednessScore) -> BuriednessScore: pass
+
+    ##
+    # \brief 
+    # \param pos 
+    # \param atoms 
+    # \return 
+    #
+    def __call__(pos: CDPL.Math.Vector3D, atoms: CDPL.Chem.AtomContainer) -> float: pass
+
+    ##
+    # \brief 
+    #
+    objectID = property(getObjectID)
+
+    ##
+    # \brief 
+    #
+    probeRadius = property(getProbeRadius, setProbeRadius)
+
+    ##
+    # \brief 
+    #
+    minVdWSurfaceDistance = property(getMinVdWSurfaceDistance, setMinVdWSurfaceDistance)
+
+    ##
+    # \brief 
+    #
+    numTestRays = property(getNumTestRays, setNumTestRays)
+
+    ##
+    # \brief FIXME!
+    #
+    atomCoordinatesFunction = property(getAtomCoordinatesFunction, setAtomCoordinatesFunction)
---
+++ Doc/Doxygen/Python-API/Source/CDPL/GRAILS/pkg_DefaultInteractionScoreGridSetCalculator.py
@@ -0,0 +1,189 @@
+#
+# This file is part of the Chemical Data Processing Toolkit
+#
+# Copyright (C) Thomas A. Seidel <thomas.seidel@univie.ac.at>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; see the file COPYING. If not, write to
+# the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
+
+##
+# \brief 
+#
+class DefaultInteractionScoreGridSetCalculator(InteractionScoreGridSetCalculator):
+
+    ##
+    # \brief Initializes the \e %DefaultInteractionScoreGridSetCalculator instance.
+    #
+    def __init__() -> None: pass
+
+    ##
+    # \brief Initializes the \e %DefaultInteractionScoreGridSetCalculator instance.
+    # \param step_size 
+    # \param x_size 
+    # \param y_size 
+    # \param z_size 
+    #
+    def __init__(step_size: float, x_size: int, y_size: int, z_size: int) -> None: pass
+
+    ##
+    # \brief Initializes the \e %DefaultInteractionScoreGridSetCalculator instance.
+    # \param x_step_size 
+    # \param y_step_size 
+    # \param z_step_size 
+    # \param x_size 
+    # \param y_size 
+    # \param z_size 
+    #
+    def __init__(x_step_size: float, y_step_size: float, z_step_size: float, x_size: int, y_size: int, z_size: int) -> None: pass
+
+    ##
+    # \brief Initializes the \e %DefaultInteractionScoreGridSetCalculator instance.
+    # \param calculator 
+    #
+    def __init__(calculator: DefaultInteractionScoreGridSetCalculator) -> None: pass
+
+    ##
+    # \brief Replaces the current state of \a self with a copy of the state of the \e %DefaultInteractionScoreGridSetCalculator instance \a calculator.
+    # \param calculator The \e %DefaultInteractionScoreGridSetCalculator instance to copy.
+    # \return \a self
+    #
+    def assign(calculator: DefaultInteractionScoreGridSetCalculator) -> DefaultInteractionScoreGridSetCalculator: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getXStepSize() -> float: pass
+
+    ##
+    # \brief 
+    # \param size 
+    #
+    def setXStepSize(size: float) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getYStepSize() -> float: pass
+
+    ##
+    # \brief 
+    # \param size 
+    #
+    def setYStepSize(size: float) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getZStepSize() -> float: pass
+
+    ##
+    # \brief 
+    # \param size 
+    #
+    def setZStepSize(size: float) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getGridXSize() -> int: pass
+
+    ##
+    # \brief 
+    # \param size 
+    #
+    def setGridXSize(size: int) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getGridYSize() -> int: pass
+
+    ##
+    # \brief 
+    # \param size 
+    #
+    def setGridYSize(size: int) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getGridZSize() -> int: pass
+
+    ##
+    # \brief 
+    # \param size 
+    #
+    def setGridZSize(size: int) -> None: pass
+
+    ##
+    # \brief 
+    # \return 
+    #
+    def getCoordinatesTransform() -> CDPL.Math.Matrix4D: pass
+
+    ##
+    # \brief 
+    # \param xform 
+    #
+    def setCoordinatesTransform(xform: CDPL.Math.Matrix4D) -> None: pass
+
+    ##
+    # \brief 
+    # \param features 
+    # \param grid_set 
+    #
+    def calculate(features: CDPL.Pharm.FeatureContainer, grid_set: CDPL.Grid.DRegularGridSet) -> None: pass
+
+    ##
+    # \brief 
+    #
+    xStepSize = property(getXStepSize, setXStepSize)
+
+    ##
+    # \brief 
+    #
+    yStepSize = property(getYStepSize, setYStepSize)
+
+    ##
+    # \brief 
+    #
+    zStepSize = property(getZStepSize, setZStepSize)
+
+    ##
+    # \brief 
+    #
+    gridXSize = property(getGridXSize, setGridXSize)
+
+    ##
+    # \brief 
+    #
+    gridYSize = property(getGridYSize, setGridYSize)
+
+    ##
+    # \brief 
+    #
+    gridZSize = property(getGridZSize, setGridZSize)
+
+    ##
+    # \brief 
+    #
+    coordinatesTransform = property(getCoordinatesTransform, setCoordinatesTransform)
