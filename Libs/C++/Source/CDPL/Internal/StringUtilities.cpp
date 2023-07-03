a75434c882581cc23401622bf8e30048b3839b4c
Thomas Seidel <thomas.seidel@univie.ac.at>
2023-07-03 20:57:30 +0200

    Undid changes causing problems with character classification

--- Libs/C++/Source/CDPL/Internal/StringUtilities.cpp
+++ Libs/C++/Source/CDPL/Internal/StringUtilities.cpp
@@ -23,6 +23,7 @@
 
 
 #include <algorithm>
+#include <locale>
 
 #include <boost/algorithm/string.hpp>
 
@@ -76,5 +77,5 @@ bool Internal::isEqualCI(const std::string& str1, const std::string& str2)
         return false;
 
     return std::equal(str1.begin(), str1.end(), str2.begin(),
-                      [](unsigned char c1, unsigned char c2) { return (std::tolower(c1) == std::tolower(c2)); });
+                      [](unsigned char c1, unsigned char c2) { return (std::tolower(c1, std::locale::classic()) == std::tolower(c2, std::locale::classic())); });
 }
