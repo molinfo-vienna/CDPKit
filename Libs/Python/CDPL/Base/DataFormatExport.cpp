49686c14d0339523056e4068e829551b0520c692
Thomas Seidel <thomas.seidel@univie.ac.at>
2023-09-06 20:55:21 +0200

    Fixed build errors on Windows

--- Libs/Python/CDPL/Base/DataFormatExport.cpp
+++ Libs/Python/CDPL/Base/DataFormatExport.cpp
@@ -26,6 +26,10 @@
 #include <iomanip>
 #include <memory>
 
+#if defined(_WIN32)
+# define _snprintf snprintf
+#endif // defined(_WIN32)
+
 #include <boost/python.hpp>
 #include <boost/python/ssize_t.hpp>
 
