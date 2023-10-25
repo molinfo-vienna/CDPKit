ed9680444fdea2184adeae38d5f95dc45900f2b1
Thomas Seidel <thomas.seidel@univie.ac.at>
2023-10-25 22:12:33 +0200

    Rearranged function calls

--- Libs/C++/Source/CDPL/Vis/CairoRenderer2D.cpp
+++ Libs/C++/Source/CDPL/Vis/CairoRenderer2D.cpp
@@ -402,14 +402,12 @@ void Vis::CairoRenderer2D::drawText(double x, double y, const std::string& txt)
 
 void Vis::CairoRenderer2D::drawEllipse(double x, double y, double width, double height)
 {
-    cairo_save(cairoContext.get());
-
     cairo_new_path(cairoContext.get());
 
+    cairo_save(cairoContext.get());
     cairo_translate(cairoContext.get(), x, y);
     cairo_scale(cairoContext.get(), 1.0, height / width);
     cairo_arc(cairoContext.get(), 0.0, 0.0, width * 0.5, 0.0, 2 * M_PI);
-
     cairo_restore(cairoContext.get());
 
     fillPath();
