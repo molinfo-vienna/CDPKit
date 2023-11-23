cd3fac9cef4bb1576b8da0469b4b534b840108a6
Thomas Seidel <thomas.seidel@univie.ac.at>
2023-11-23 13:48:55 +0100

    Code cleanup

--- Libs/Python/CDPL/Vis/ReactionFunctionExport.cpp
+++ Libs/Python/CDPL/Vis/ReactionFunctionExport.cpp
@@ -32,55 +32,18 @@
 #include "FunctionExports.hpp"
 
 
-#define MAKE_REACTION_FUNC_WRAPPERS(TYPE, FUNC_SUFFIX)     \
-TYPE get##FUNC_SUFFIX##Wrapper(CDPL::Chem::Reaction& rxn)  \
-{                                                          \
-    return CDPL::Vis::get##FUNC_SUFFIX(rxn);               \
-}                                                          \
-                                                           \
-bool has##FUNC_SUFFIX##Wrapper(CDPL::Chem::Reaction& rxn)  \
-{                                                          \
-    return CDPL::Vis::has##FUNC_SUFFIX(rxn);               \
-}
-
 #define EXPORT_REACTION_FUNCS_INT_REF(FUNC_SUFFIX, ARG_NAME)                                                \
-python::def("get"#FUNC_SUFFIX, &get##FUNC_SUFFIX##Wrapper, python::arg("rxn"),                              \
+python::def("get"#FUNC_SUFFIX, &Vis::get##FUNC_SUFFIX, python::arg("rxn"),                                  \
             python::return_internal_reference<1>());                                                        \
-python::def("has"#FUNC_SUFFIX, &has##FUNC_SUFFIX##Wrapper, python::arg("rxn"));                             \
+python::def("has"#FUNC_SUFFIX, &Vis::has##FUNC_SUFFIX, python::arg("rxn"));                                 \
 python::def("clear"#FUNC_SUFFIX, &Vis::clear##FUNC_SUFFIX, python::arg("rxn"));                             \
 python::def("set"#FUNC_SUFFIX, &Vis::set##FUNC_SUFFIX, (python::arg("rxn"), python::arg(#ARG_NAME))); 
 
 #define EXPORT_REACTION_FUNCS(FUNC_SUFFIX, ARG_NAME)                                                        \
-python::def("get"#FUNC_SUFFIX, &get##FUNC_SUFFIX##Wrapper, python::arg("rxn"));                             \
-python::def("has"#FUNC_SUFFIX, &has##FUNC_SUFFIX##Wrapper, python::arg("rxn"));                             \
+python::def("get"#FUNC_SUFFIX, &Vis::get##FUNC_SUFFIX, python::arg("rxn"));                                 \
+python::def("has"#FUNC_SUFFIX, &Vis::has##FUNC_SUFFIX, python::arg("rxn"));                                 \
 python::def("clear"#FUNC_SUFFIX, &Vis::clear##FUNC_SUFFIX, python::arg("rxn"));                             \
 python::def("set"#FUNC_SUFFIX, &Vis::set##FUNC_SUFFIX, (python::arg("rxn"), python::arg(#ARG_NAME))); 
-
-
-namespace
-{
-
-    MAKE_REACTION_FUNC_WRAPPERS(unsigned int, ArrowStyle)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::Color&, ArrowColor)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::SizeSpecification&, ArrowLength)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::SizeSpecification&, ArrowHeadLength)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::SizeSpecification&, ArrowHeadWidth)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::SizeSpecification&, ArrowShaftWidth)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::SizeSpecification&, ArrowLineWidth)
-    MAKE_REACTION_FUNC_WRAPPERS(unsigned int, ComponentLayout)
-    MAKE_REACTION_FUNC_WRAPPERS(unsigned int, ComponentLayoutDirection)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::SizeSpecification&, ComponentMargin)
-    MAKE_REACTION_FUNC_WRAPPERS(bool, ShowReactants)
-    MAKE_REACTION_FUNC_WRAPPERS(bool, ShowAgents)
-    MAKE_REACTION_FUNC_WRAPPERS(bool, ShowProducts)
-    MAKE_REACTION_FUNC_WRAPPERS(unsigned int, AgentAlignment)
-    MAKE_REACTION_FUNC_WRAPPERS(unsigned int, AgentLayout)
-    MAKE_REACTION_FUNC_WRAPPERS(unsigned int, AgentLayoutDirection)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::Color&, PlusSignColor)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::SizeSpecification&, PlusSignSize)
-    MAKE_REACTION_FUNC_WRAPPERS(const CDPL::Vis::SizeSpecification&, PlusSignLineWidth)
-    MAKE_REACTION_FUNC_WRAPPERS(bool, ShowPlusSigns)
-}
 
 
 void CDPLPythonVis::exportReactionFunctions()
