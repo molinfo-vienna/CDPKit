d394f9a5e1c4347ef6442f6cb8143dea9159cd5d
Thomas Seidel <thomas.seidel@univie.ac.at>
2024-03-29 11:14:20 +0100

    Code cleanup

--- Libs/Python/CDPL/Vis/QtBackend.py
+++ Libs/Python/CDPL/Vis/QtBackend.py
@@ -68,7 +68,7 @@ class ToQPainterPathConverter(Vis.Path2DConverter):
 
     def __init__(self, path, qt_path):
         Vis.Path2DConverter.__init__(self)
-        self.__qPainterPath = qt_path
+        self._qPainterPath = qt_path
         
         if qt_path.elementCount() > 0:
             QtGui.QPainterPath().swap(qt_path)
@@ -82,16 +82,16 @@ class ToQPainterPathConverter(Vis.Path2DConverter):
         path.convert(self)
 
     def moveTo(self, x, y):
-        self.__qPainterPath.moveTo(x, y)
+        self._qPainterPath.moveTo(x, y)
 
     def arcTo(self, cx, cy, rx, ry, start_ang, sweep):
-        self.__qPainterPath.arcTo(cx - rx, cy - ry, 2.0 * rx, 2.0 * ry, -start_ang, -sweep)
+        self._qPainterPath.arcTo(cx - rx, cy - ry, 2.0 * rx, 2.0 * ry, -start_ang, -sweep)
 
     def lineTo(self, x, y):
-        self.__qPainterPath.lineTo(x, y)
+        self._qPainterPath.lineTo(x, y)
 
     def closePath(self):
-        self.__qPainterPath.closeSubpath()
+        self._qPainterPath.closeSubpath()
 
 
 class QtObjectFactory(object):
@@ -188,50 +188,63 @@ class QtFontMetrics(Vis.FontMetrics):
         "__init__(QtFontMetrics self, PyQt5.QtGui.QPaintDevice paint_dev) -> None :"
         Vis.FontMetrics.__init__(self)
 
-        self.__qPaintDevice = paint_dev
-        self.__qFontMetrics = QtGui.QFontMetricsF(QtGui.QFont(), paint_dev)
+        self._qPaintDevice = paint_dev
+        self._qFontMetrics = QtGui.QFontMetricsF(QtGui.QFont(), paint_dev)
 
     def setFont(self, font):
         "setFont(QtFontMetrics self, Vis.Font font) -> None :"
-        self.__qFontMetrics = QtGui.QFontMetricsF(QtObjectFactory.createQFont(font), self.__qPaintDevice)
+        self._qFontMetrics = QtGui.QFontMetricsF(QtObjectFactory.createQFont(font), self._qPaintDevice)
             
     def getAscent(self):
         "getAscent(QtFontMetrics self) -> float :"
-        return self.__qFontMetrics.ascent()
+        return self._qFontMetrics.ascent()
     
     def getDescent(self):
         "getDescent(QtFontMetrics self) -> float :"
-        return self.__qFontMetrics.descent()
+        return self._qFontMetrics.descent()
     
     def getHeight(self):
         "getHeight(QtFontMetrics self) -> float :"
-        return self.__qFontMetrics.height()
+        return self._qFontMetrics.height()
     
     def getLeading(self):
         "getLeading(QtFontMetrics self) -> float :"
-        return self.__qFontMetrics.leading()
+        return self._qFontMetrics.leading()
     
     def getWidth(self, string):
         "getWidth(QtFontMetrics self) -> float :"
-        return self.__qFontMetrics.width(string)
+        return self._qFontMetrics.width(string)
 
     def getBounds(self, string, bounds):
         "getBounds(QtFontMetrics self, str string, Vis.Rectangle2D bounds) -> float :"
         if len(string) == 1:
-            ch_bounds = self.__qFontMetrics.boundingRectChar(string)
+            if string.isspace():
+                bounds.setBounds(0.0, 0.0, self._qFontMetrics.width(string), 0.0)
+                return
+
+            ch_bounds = self._qFontMetrics.boundingRectChar(string)
             bounds.setBounds(ch_bounds.left(), ch_bounds.top(), ch_bounds.right(), ch_bounds.bottom())            
             return
 
-        str_bounds = self.__qFontMetrics.boundingRect(string)
+        str_bounds = self._qFontMetrics.boundingRect(string)
             
         bounds.setMin(str_bounds.left(), 0.0);
         bounds.setMax(str_bounds.right(), 0.0);
 
         for c in string:
-            str_bounds = self.__qFontMetrics.boundingRectChar(c)
+            str_bounds = self._qFontMetrics.boundingRectChar(c)
 
             bounds.addPoint(bounds.getMin()[0], str_bounds.top());
             bounds.addPoint(bounds.getMax()[0], str_bounds.bottom());
+
+        if not string:
+            return
+
+        if string[-1].isspace():
+            bounds.addPoint(self._qFontMetrics.width(string), 0.0)
+
+        if string.isspace():
+            bounds.addPoint(0.0, 0.0)
 
 
 class QtRenderer2D(Vis.Renderer2D):
@@ -239,57 +252,57 @@ class QtRenderer2D(Vis.Renderer2D):
     def __init__(self, painter):
         "__init__(QtRenderer2D self, PyQt5.QtGui.QPainter painter) -> None :"
         Vis.Renderer2D.__init__(self)
-        self.__qPainter = painter
-        self.__qPainterPath = None
-        self.__qPolygon = None
+        self._qPainter = painter
+        self._qPainterPath = None
+        self._qPolygon = None
         
     def saveState(self):
         "saveState(QtRenderer2D self) -> None :"
-        self.__qPainter.save()
+        self._qPainter.save()
     
     def restoreState(self):
         "restoreState(QtRenderer2D self) -> None :"
-        self.__qPainter.restore()
+        self._qPainter.restore()
     
     def setTransform(self, xform):
         "setTransform(QtRenderer2D self, Math.Matrix3D xform) -> None :"
-        self.__qPainter.setWorldTransform(QtGui.QTransform(xform(0, 0), xform(1, 0), xform(2, 0), 
+        self._qPainter.setWorldTransform(QtGui.QTransform(xform(0, 0), xform(1, 0), xform(2, 0), 
                                                            xform(0, 1), xform(1, 1), xform(2, 1), 
                                                            xform(0, 2), xform(1, 2), xform(2, 2)), False)
     
     def transform(self, xform):
         "transform(QtRenderer2D self, Math.Matrix3D xform) -> None :"
-        self.__qPainter.setWorldTransform(QtGui.QTransform(xform(0, 0), xform(1, 0), xform(2, 0), 
+        self._qPainter.setWorldTransform(QtGui.QTransform(xform(0, 0), xform(1, 0), xform(2, 0), 
                                                            xform(0, 1), xform(1, 1), xform(2, 1), 
                                                            xform(0, 2), xform(1, 2), xform(2, 2)), True)
 
     def setPen(self, pen):
         "setPen(QtRenderer2D self, Vis.Pen pen) -> None :"
-        self.__qPainter.setPen(QtObjectFactory.createQPen(pen))
+        self._qPainter.setPen(QtObjectFactory.createQPen(pen))
     
     def setBrush(self, brush):
         "setBrush(QtRenderer2D self, Vis.Brush brush) -> None :"
-        self.__qPainter.setBrush(QtObjectFactory.createQBrush(brush))
+        self._qPainter.setBrush(QtObjectFactory.createQBrush(brush))
     
     def setFont(self, font):
         "setFont(QtRenderer2D self, Vis.Font font) -> None :"
-        self.__qPainter.setFont(QtObjectFactory.createQFont(font))
+        self._qPainter.setFont(QtObjectFactory.createQFont(font))
     
     def drawRectangle(self, x, y, width, height):
         "drawRectangle(QtRenderer2D self, float x, float y, float width, float height) -> None :"
-        self.__qPainter.drawRect(QtCore.QRectF(x, y, width, height))
+        self._qPainter.drawRect(QtCore.QRectF(x, y, width, height))
     
     def drawPolygon(self, points):
         "drawPolygon(QtRenderer2D self, Math.Vector2DArray points) -> None :"
-        self.__qPainter.drawPolygon(self.__convertToQPolygon(points))
+        self._qPainter.drawPolygon(self._convertToQPolygon(points))
             
     def drawLine(self, x1, y1, x2, y2):
         "drawLine(QtRenderer2D self, float x1, float y1, float x2, float y2) -> None :"
-        self.__qPainter.drawLine(QtCore.QPointF(x1, y1), QtCore.QPointF(x2, y2))
+        self._qPainter.drawLine(QtCore.QPointF(x1, y1), QtCore.QPointF(x2, y2))
     
     def drawPolyline(self, points):
         "drawPolyline(QtRenderer2D self, Math.Vector2DArray points) -> None :"
-        self.__qPainter.drawPolyline(self.__convertToQPolygon(points))
+        self._qPainter.drawPolyline(self._convertToQPolygon(points))
     
     def drawLineSegments(self, points):
         "drawLineSegments(QtRenderer2D self, Math.Vector2DArray points) -> None :"
@@ -297,51 +310,51 @@ class QtRenderer2D(Vis.Renderer2D):
             point1 = points[i]
             point2 = points[i + 1]
             
-            self.__qPainter.drawLine(point1[0], point1[1], point2[0], point2[1])
+            self._qPainter.drawLine(point1[0], point1[1], point2[0], point2[1])
     
     def drawPoint(self, x, y):
         "drawPoint(QtRenderer2D self, float x, float y) -> None :"
-        self.__qPainter.drawPoint(QtCore.QPointF(x, y))
+        self._qPainter.drawPoint(QtCore.QPointF(x, y))
         
     def drawEllipse(self, x, y, width, height):
         "drawEllipse(QtRenderer2D self, float x, float y, float width, float height) -> None :"
-        self.__qPainter.drawEllipse(QtCore.QPointF(x, y), width * 0.5, height * 0.5)
+        self._qPainter.drawEllipse(QtCore.QPointF(x, y), width * 0.5, height * 0.5)
 
     def drawText(self, x, y, txt):
         "drawText(QtRenderer2D self, float x, float y, str txt) -> None :"
-        self.__qPainter.drawText(QtCore.QPointF(x, y), txt)
+        self._qPainter.drawText(QtCore.QPointF(x, y), txt)
 
     def drawPath(self, path):
         "drawPath(QtRenderer2D self, Vis.Path2D path) -> None :"
         if path.isEmpty():
             return
         
-        if self.__qPainterPath == None:
-            self.__qPainterPath = QtGui.QPainterPath()
+        if self._qPainterPath == None:
+            self._qPainterPath = QtGui.QPainterPath()
 
-        self.__qPainter.drawPath(QtObjectFactory.createQPainterPath(path, self.__qPainterPath))
+        self._qPainter.drawPath(QtObjectFactory.createQPainterPath(path, self._qPainterPath))
 
     def setClipPath(self, path):
         "setClipPath(QtRenderer2D self, Vis.Path2D path) -> None :"
         if path.isEmpty():
             return
 
-        if self.__qPainterPath == None:
-            self.__qPainterPath = QtGui.QPainterPath()
+        if self._qPainterPath == None:
+            self._qPainterPath = QtGui.QPainterPath()
             
-        self.__qPainter.setClipPath(QtObjectFactory.createQPainterPath(path, self.__qPainterPath))
+        self._qPainter.setClipPath(QtObjectFactory.createQPainterPath(path, self._qPainterPath))
         
     def clearClipPath(self):
         "clearClipPath(QtRenderer2D self) -> None :"
-        self.__qPainter.setClipping(False)
+        self._qPainter.setClipping(False)
 
-    def __convertToQPolygon(self, points):
-        if self.__qPolygon == None:
-            self.__qPolygon = QtGui.QPolygonF()
+    def _convertToQPolygon(self, points):
+        if self._qPolygon == None:
+            self._qPolygon = QtGui.QPolygonF()
 
-        self.__qPolygon.clear()
+        self._qPolygon.clear()
 
         for point in points:
-            self.__qPolygon.append(QtCore.QPointF(point[0], point[1]))
+            self._qPolygon.append(QtCore.QPointF(point[0], point[1]))
 
-        return self.__qPolygon
+        return self._qPolygon
