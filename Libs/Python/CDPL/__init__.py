3cd585002f1bc5689dacca33d133fa70ffd33e10
Thomas Seidel <thomas.seidel@univie.ac.at>
2025-03-05 14:23:23 +0100

    In Python CDPL version is now available as attribute '__version__' of package CDPL

--- Libs/Python/CDPL/__init__.py
+++ Libs/Python/CDPL/__init__.py
@@ -35,3 +35,5 @@ if sys.version_info[1] >= 8 and platform.system() == 'Windows':
         os.add_dll_directory(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), '@CDPKIT_EXECUTABLE_INSTALL_DIR@'))
 
 from ._cdpl import *
+
+__version__ = VERSION_STRING
