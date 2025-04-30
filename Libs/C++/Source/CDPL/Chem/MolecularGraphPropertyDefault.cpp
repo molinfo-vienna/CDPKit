cf3a4860d2f950f925402893d11a958aa41ae750
Thomas Seidel <thomas.seidel@univie.ac.at>
2025-04-30 13:26:06 +0200

    Code formatting

--- Libs/C++/Source/CDPL/Chem/MolecularGraphPropertyDefault.cpp
+++ Libs/C++/Source/CDPL/Chem/MolecularGraphPropertyDefault.cpp
@@ -39,20 +39,20 @@ namespace CDPL
         namespace MolecularGraphPropertyDefault
         {
 
-            const std::string NAME                                     = "";
-            const std::string COMMENT                                  = "";
-            const double STOICHIOMETRIC_NUMBER                         = 1.0;
-            const MatchConstraintList::SharedPointer MATCH_CONSTRAINTS = MatchConstraintList::SharedPointer(new MatchConstraintList());
-            const unsigned int MDL_CTAB_VERSION                        = MDLDataFormatVersion::UNDEF;
-            const std::string MDL_PROGRAM_NAME                         = "CDPL";
-            const std::string MDL_USER_INITIALS                        = "";
-            const std::size_t MDL_DIMENSIONALITY                       = 0;
-            const double MDL_ENERGY                                    = 0.0 ;
-            const bool MDL_CHIRAL_FLAG                                 = false;
-            const long MDL_SCALING_FACTOR1                             = 1;
-            const double MDL_SCALING_FACTOR2                           = 1;
-            const unsigned int MOL2_MOLECULE_TYPE                      = MOL2MoleculeType::UNKNOWN;
-            const unsigned int MOL2_CHARGE_TYPE                        = MOL2ChargeType::UNKNOWN;
+            const std::string                        NAME                  = "";
+            const std::string                        COMMENT               = "";
+            const double                             STOICHIOMETRIC_NUMBER = 1.0;
+            const MatchConstraintList::SharedPointer MATCH_CONSTRAINTS     = MatchConstraintList::SharedPointer(new MatchConstraintList());
+            const unsigned int                       MDL_CTAB_VERSION      = MDLDataFormatVersion::UNDEF;
+            const std::string                        MDL_PROGRAM_NAME      = "CDPL";
+            const std::string                        MDL_USER_INITIALS     = "";
+            const std::size_t                        MDL_DIMENSIONALITY    = 0;
+            const double                             MDL_ENERGY            = 0.0;
+            const bool                               MDL_CHIRAL_FLAG       = false;
+            const long                               MDL_SCALING_FACTOR1   = 1;
+            const double                             MDL_SCALING_FACTOR2   = 1;
+            const unsigned int                       MOL2_MOLECULE_TYPE    = MOL2MoleculeType::UNKNOWN;
+            const unsigned int                       MOL2_CHARGE_TYPE      = MOL2ChargeType::UNKNOWN;
         }
 
         void initMolecularGraphPropertyDefaults() {}
